9ee982d810ae49af2265413e0a9421e72b7cefd4
Nuhu Sulemana <turosung@gmail.com>
2023-07-04 21:42:00 +0100

    Initial Commit

---
+++ PlanYourDay/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlanYourDay
+//
+//  Created by Nuhu Sulemana on 04/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlanYourDay/PlanYourDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanYourDayApp.swift
+//  PlanYourDay
+//
+//  Created by Nuhu Sulemana on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlanYourDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
