ef974296d943466f8f37bdaf23ec8a2b16368dec
Nuhu Sulemana <turosung@gmail.com>
2023-07-18 16:14:43 +0100

    Added UserDefaults so that data is stored and retrieved when app is launched

--- PlanYourDay/ViewModels/ListViewModel.swift
+++ PlanYourDay/ViewModels/ListViewModel.swift
@@ -17,21 +17,25 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the 1st item", isCompleted: false),
-            ItemModel(title: "This is the 2nd item", isCompleted: true),
-            ItemModel(title: "Last item on the list", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
+        
     }
-    
     func deleteItem(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
     }
@@ -49,5 +53,12 @@ class ListViewModel: ObservableObject {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
         }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
+
     }
 }
